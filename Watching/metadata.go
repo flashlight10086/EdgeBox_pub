6c80fea5ab1bd86d404d8c59bf456721cd9e68ef
flashlight10086 <41638160+flashlight10086@users.noreply.github.com>
2019-05-29 19:23:13 +0800

    Update metadata.go

--- Watching/metadata.go
+++ Watching/metadata.go
@@ -4,17 +4,17 @@ import "github.com/project-flogo/core/data/coerce"
 
 
 type Output struct {
-	serial string `md:"serial"`
+	Serial string `md:"serial"`
 }
 
 func (o *Output) FromMap(values map[string]interface{}) error {
 	strVal, _ := coerce.ToString(values["serial"])
-	o.serial = strVal
+	o.Serial = strVal
 	return nil
 }
 
 func (o *Output) ToMap() map[string]interface{} {
 	return map[string]interface{}{
-		"serial": o.serial,
+		"serial": o.Serial,
 	}
 }
