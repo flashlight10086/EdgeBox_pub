5ca981b1c65aaa5d1297202bed35e027eb2596f4
flashlight10086 <41638160+flashlight10086@users.noreply.github.com>
2019-06-27 00:15:11 -0400

    Update activity.go

--- Watching/activity.go
+++ Watching/activity.go
@@ -83,7 +83,7 @@ func (a *Activity) Eval(ctx activity.Context) (done bool, err error) {
 	if jsonString, err := json.Marshal(config); err == nil {
            fmt.Println("================struct to json str==")
            fmt.Println(string(jsonString))
-	   output := &Output{Serial: jsonString}
+		output := &Output{Serial: string(jsonString)}
         }
 	//output := &Output{Serial: "1"}//should be serial of the record in the database
 	err = ctx.SetOutputObject(output)
