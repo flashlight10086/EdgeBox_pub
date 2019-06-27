bca0c1004d6e433e36ffaac9c07c498318711d27
flashlight10086 <41638160+flashlight10086@users.noreply.github.com>
2019-06-26 21:30:18 -0400

    Update metadata.go

--- GenderAnalysis/metadata.go
+++ GenderAnalysis/metadata.go
@@ -19,6 +19,21 @@ func (r *Input) ToMap() map[string]interface{} {
 	}
 }
 
+type Output struct {
+	AgeJson string `md:"genderJson"`
+}
+
+func (o *Output) FromMap(values map[string]interface{}) error {
+	strVal, _ := coerce.ToString(values["genderJson"])
+	o.GenderJson = strVal
+	return nil
+}
+
+func (o *Output) ToMap() map[string]interface{} {
+	return map[string]interface{}{
+		"genderJson": o.GenderJson,
+	}
+}
 
 
 
