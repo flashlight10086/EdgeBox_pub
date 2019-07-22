8412408000bfd78629026e22842f8d4478e1c896
flashlight10086 <41638160+flashlight10086@users.noreply.github.com>
2019-07-23 02:22:44 +0800

    Update activity.go

--- AgeAnalysis/activity.go
+++ AgeAnalysis/activity.go
@@ -72,7 +72,7 @@ func (a *Activity) Eval(ctx activity.Context) (done bool, err error) {
 				Y1:y1,
 				X2:x2,
 				Y2:y2,
-			        Result:"dummy-result"
+			        Result:"dummy-result",
 			      },
 			},	   
 	}
